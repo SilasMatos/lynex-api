604f4ac9b7967481735cd64bf1b0566b320fd4d5
Silas <silas.progamacao@gmail.com>
2026-03-06 13:42:04 -0300

    add domain

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,8 @@
-import  {betterAuth} from "better-auth"
-import {drizzleAdapter} from "better-auth/adapters/drizzle"
-import {openAPI  } from "better-auth/plugins"
-import {db} from "@/database/client"
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { openAPI } from "better-auth/plugins"
+import { db } from "@/database/client"
+
 export const auth = betterAuth({
     basePath: "/auth",
     baseURL: process.env.BETTER_AUTH_URL,
@@ -15,33 +16,37 @@ export const auth = betterAuth({
     },
     plugins: [
         openAPI()
-        
     ],
-    database: drizzleAdapter(
-    db, {
+    database: drizzleAdapter(db, {
         provider: "pg",
         usePlural: true,
-
     }),
     trustedOrigins: ["http://localhost:3333", "http://localhost:3000", "https://lynex-cli.vercel.app"],
     advanced: {
+        crossSubDomainCookies: {
+            enabled: false,
+        },
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
+        },
         database: {
-            generateId: false // Desativa a geração automática de IDs
-        }
+            generateId: false,
+        },
     },
-    emailAndPassword:{
-        enabled: true, // Habilita autenticação por email e senha
-        autoSignIn: true,//autentica o usuário automaticamente após o cadastro
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true,
         password: {
-            hash: (password: string) => Bun.password.hash(password), // Função para hashear a senha
-            verify: ({password, hash }) => Bun.password.verify(password, hash), // Função para verificar a senha
-        }
-    } ,
+            hash: (password: string) => Bun.password.hash(password),
+            verify: ({ password, hash }) => Bun.password.verify(password, hash),
+        },
+    },
     session: {
-      expiresIn: 60 * 60 * 24 * 7, // Sessão expira em 7 dias
-      cookieCache: {
-        maxAge: 60 * 60 * 24 * 7, // Cookie expira em 7 dias
-        enabled: true, // Habilita cache de cookies
-      }
-    }
+        expiresIn: 60 * 60 * 24 * 7,
+        cookieCache: {
+            maxAge: 60 * 60 * 24 * 7,
+            enabled: true,
+        },
+    },
 })
