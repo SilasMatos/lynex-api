dc4ba5b782f7751d83b53a4f8c87c99c6cb19ab4
Silas <silas.progamacao@gmail.com>
2026-03-09 00:54:39 -0300

    .

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
-import { openAPI, oAuthProxy } from "better-auth/plugins"
+import { openAPI } from "better-auth/plugins"
 import { db } from "@/database/client"
 
 export const auth = betterAuth({
@@ -16,7 +16,6 @@ export const auth = betterAuth({
     },
     plugins: [
         openAPI(),
-        oAuthProxy({ productionURL: 'https://lynex-cli.vercel.app' }),
     ],
     database: drizzleAdapter(db, {
         provider: "pg",
